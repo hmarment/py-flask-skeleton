46a9d6744396ba99a8fda5b53c0118ffbccfdce2
Henry Marment <henry@adsquare.com>
2019-02-01 12:32:02 +0100

    add alembic migration script

---
+++ py_flask_skeleton/manage.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import with_statement, print_function, absolute_import
+
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+from server import app, db
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
